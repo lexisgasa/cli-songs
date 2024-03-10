09d3af4954ba0e1255f13aebadebca26ee73a2d1
Mario González <mario@mariogl.com>
2024-03-10 08:43:07 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import askUser from "./askUser.js";
+
+// Aquí tienes un ejemplo de cómo pedir un dato al usuario
+// En la variable age se guardará el dato que el usuario introduzca
+const age = askUser("Enter your age: ");
+
+// Sólo ten en cuenta que el dato que el usuario introduzca siempre será de tipo string
+// Si necesitas que sea de otro tipo, tendrás que convertirlo
+
+// Borra todo este código menos la línea 1
+// y no borres el archivo askUser.ts, que es el que contiene la función askUser
