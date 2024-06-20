05a56d0398d117115bf9debc40ef4ed72a2542ae
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-06-20 18:11:23 +0200

    array creado, condición if añadida

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 import askUser from "./askUser.js";
+
+const songsList = [
+  "",
+  "Bat country",
+  "Bring me to life",
+  "In the end",
+  "Emerald Sword",
+  "Venom of Venus",
+];
+
+const songPosition = parseInt(
+  askUser("Inserta una posición para ver la canción asociada a ella: ")
+);
+
+if (!songsList[songPosition]) {
+  console.log(`No hay ninguna canción en la posición ${songPosition}`);
+} else {
+  console.log(
+    `La canción en la posición ${songPosition} es: ${songsList[songPosition]}`
+  );
+}
